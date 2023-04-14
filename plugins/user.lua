fe85921897a8d28763c67ba6aae35caa145771a1
melonges <mellonges@icloud.com>
2023-04-14 14:53:23 +0300

    fix lazy load

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,7 +2,7 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   {"andweeb/presence.nvim", lazy = false},
-  {"folke/todo-comments.nvim", lazy = false},
+  -- {"folke/todo-comments.nvim", lazy = false},
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
