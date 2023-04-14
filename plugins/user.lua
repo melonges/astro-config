187971be699ad8674b964950c36f6cb1017bfc64
melonges <mellonges@icloud.com>
2023-04-14 20:17:14 +0300

    remove todo-plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,15 +2,15 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   {"andweeb/presence.nvim", lazy = false},
-  {"folke/todo-comments.nvim", lazy = false,
-  requires = "nvim-lua/plenary.nvim",
-  config = function()
-    require("todo-comments").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  end},
+  -- {"folke/todo-comments.nvim", lazy = false,
+  -- requires = "nvim-lua/plenary.nvim",
+  -- config = function()
+  --   require("todo-comments").setup {
+  --     -- your configuration comes here
+  --     -- or leave it empty to use the default settings
+  --     -- refer to the configuration section below
+  --   }
+  -- end},
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
